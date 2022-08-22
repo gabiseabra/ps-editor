ce534052835d7485bd62e6b4e2964462df1c099b
gabiseabra <gabriela.lima.se@gmail.com>
2022-08-22 15:00:00 -0300

    fix show instance of Block

--- src/Editor/Block/AST.purs
+++ src/Editor/Block/AST.purs
@@ -11,6 +11,7 @@ import Data.Eq.Generic (genericEq)
 import Data.Generic.Rep (class Generic)
 import Data.Newtype (class Newtype)
 import Data.Show.Generic (genericShow)
+import Data.String as String
 import Data.Tuple.Nested (type (/\), (/\))
 import Editor.Lexer (Parser)
 import Matryoshka (cata)
@@ -74,7 +75,12 @@ instance bifunctorBlock :: Bifunctor Block where
   bimap f g (B c0) = B $ c0 `flip fix` \h c -> g (head c) :< bimap f h (tail c)
 
 instance showBlock :: ( Show a, Show r ) => Show (Block a r) where
-  show (B c) = c `flip cata` \(EnvT (r /\ body)) -> show r <> ":<(" <> show body <> ")"
+  show (B c) = c `flip cata` \(EnvT (r /\ block)) ->
+    show r <> ":<(" <> (
+      case block of
+        (BlockF bs) -> "BlockF " <> "[" <> String.joinWith ", " bs <> "]"
+        b -> show b
+    ) <> ")"
 
 class BlockType a where
   blockKind :: forall proxy. proxy a -> BlockKind
